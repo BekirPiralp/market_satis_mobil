a51872b93bc4fc245c5ff0ac5d02f5003cf9a831
BekirPiralp <bpir33@gmail.com>
2021-06-25 02:30:52 +0300

    ufak bir son dokunuş

--- lib/views/temelWidget.dart
+++ lib/views/temelWidget.dart
@@ -10,12 +10,15 @@ class TemelWidget extends StatefulWidget {
   temelWidget createState() => temelWidget();
 }
 
-class temelWidget extends State {
+class temelWidget extends State with SingleTickerProviderStateMixin{
   //class temelWidget<T extends StatefulWidget> extends State<T> {
   String baslik = "";
   Widget govde = Text("al abim");
   Veri veri;
   String tc;
+
+  Animation animasyon;
+  AnimationController animasyonKontrol;
 
   void veriSet(BuildContext context) {
     //sınıf içerisinde veriyi kullanırken bu fonksiypnu kullanması gerek
@@ -28,30 +31,50 @@ class temelWidget extends State {
     return _build(context, this.baslik);
   }
 
+  @override
+  void initState(){
+    super.initState();
+    animasyonKontrol =
+        AnimationController(duration:Duration(seconds: 2), vsync: this);
+    animasyon= Tween(begin: 1.0,end: 0.0).animate(CurvedAnimation(
+        parent: animasyonKontrol,
+        curve: Curves.fastOutSlowIn));
+    animasyonKontrol.forward();
+  }
+
   Widget _build(BuildContext context, String baslik) {
+    final double width = MediaQuery.of(context).size.width;
     this.set(baslik: baslik, govde: this.govde);
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Uni(),
-          title: Text(this.baslik),
-          centerTitle: true,
-          actions: [
-            IconButton(
-                icon: Icon(Icons.waves),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => MenuGoster(),
-                        settings: RouteSettings(
-                          arguments: this.veri,
-                        ),
-                      ));
-                }),
-          ],
-        ),
-        body: Temel().sinirDuzenle(govde),
+      home: AnimatedBuilder(
+        animation: animasyonKontrol,
+        builder: (BuildContext context, Widget child){
+          return Scaffold(
+            appBar: AppBar(
+              leading: Uni(),
+              title: Text(this.baslik),
+              centerTitle: true,
+              actions: [
+                IconButton(
+                    icon: Icon(Icons.waves),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => MenuGoster(),
+                            settings: RouteSettings(
+                              arguments: this.veri,
+                            ),
+                          ));
+                    }),
+              ],
+            ),
+            body: Transform(
+              transform: Matrix4.translationValues(animasyon.value*width, 0, 0),
+              child: Temel().sinirDuzenle(govde),
+            ),
+          );
+        },
       ),
       theme: ThemeData(
         primarySwatch: Colors.blue,
