75841c5d64f90be9923062d476195b9bee89108c
BekirPiralp <bpir33@gmail.com>
2021-06-25 03:39:39 +0300

    minim innacık bir değişim

--- lib/main/myHomePage.dart
+++ lib/main/myHomePage.dart
@@ -12,28 +12,48 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState(routes,context);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
   BuildContext context;
-
   Routes routes;
+
+  Animation animasyon;
+  AnimationController animasyonKontrol;
 
   _MyHomePageState(Routes routes,this.context){
     this.routes=routes;
   }
 
   @override
-  Widget build(BuildContext context) {
+  void initState(){
+    super.initState();
+    animasyonKontrol =
+        AnimationController(duration:Duration(seconds: 2), vsync: this);
+    animasyon= Tween(begin: 1.0,end: 0.0).animate(CurvedAnimation(
+        parent: animasyonKontrol,
+        curve: Curves.fastOutSlowIn));
+    animasyonKontrol.forward();
+  }
 
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
     //geridönüş
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          widget.title,
-          textAlign: TextAlign.center,
-        ),
-      ),
-      body:Govde(),
+    return AnimatedBuilder(
+        animation: animasyonKontrol,
+        builder: (BuildContext context, Widget child){
+             return Scaffold(
+              appBar: AppBar(
+                centerTitle: true,
+                title: Text(
+                  widget.title,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              body: Transform(
+              transform: Matrix4.translationValues(animasyon.value*width, 0, 0),
+                child:Govde(),),
+            );},
     );
   }
 }
--- lib/views/temelWidget.dart
+++ lib/views/temelWidget.dart
@@ -10,15 +10,14 @@ class TemelWidget extends StatefulWidget {
   temelWidget createState() => temelWidget();
 }
 
-class temelWidget extends State with SingleTickerProviderStateMixin{
+class temelWidget extends State {
   //class temelWidget<T extends StatefulWidget> extends State<T> {
   String baslik = "";
   Widget govde = Text("al abim");
   Veri veri;
   String tc;
 
-  Animation animasyon;
-  AnimationController animasyonKontrol;
+
 
   void veriSet(BuildContext context) {
     //sınıf içerisinde veriyi kullanırken bu fonksiypnu kullanması gerek
@@ -31,25 +30,13 @@ class temelWidget extends State with SingleTickerProviderStateMixin{
     return _build(context, this.baslik);
   }
 
-  @override
-  void initState(){
-    super.initState();
-    animasyonKontrol =
-        AnimationController(duration:Duration(seconds: 2), vsync: this);
-    animasyon= Tween(begin: 1.0,end: 0.0).animate(CurvedAnimation(
-        parent: animasyonKontrol,
-        curve: Curves.fastOutSlowIn));
-    animasyonKontrol.forward();
-  }
+
 
   Widget _build(BuildContext context, String baslik) {
-    final double width = MediaQuery.of(context).size.width;
+
     this.set(baslik: baslik, govde: this.govde);
     return MaterialApp(
-      home: AnimatedBuilder(
-        animation: animasyonKontrol,
-        builder: (BuildContext context, Widget child){
-          return Scaffold(
+      home: Scaffold(
             appBar: AppBar(
               leading: Uni(),
               title: Text(this.baslik),
@@ -69,13 +56,9 @@ class temelWidget extends State with SingleTickerProviderStateMixin{
                     }),
               ],
             ),
-            body: Transform(
-              transform: Matrix4.translationValues(animasyon.value*width, 0, 0),
-              child: Temel().sinirDuzenle(govde),
-            ),
-          );
-        },
-      ),
+            body: Temel().sinirDuzenle(govde),
+          ),
+
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
